8f062ac00c1086e682617e6630edf88cdf8d597c
jaybell <jaycooperbell@gmail.com>
2023-11-04 08:40:37 -0700

    add ngrx packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideEffects} from "@ngrx/effects";
+import {provideStore} from "@ngrx/store";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),]
 };
