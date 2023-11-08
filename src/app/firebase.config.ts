d5fe2a69cf4be40bbe939787569fe8ff7cac2425
jaybell <jaycooperbell@gmail.com>
2023-11-08 07:51:26 -0800

    add firebase configs

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyADxvTcyJ7JlCBb1X7iwWUUdBSGD45MGq0',
+  authDomain: 'fir-ssr-demo.firebaseapp.com',
+  projectId: 'fir-ssr-demo',
+  storageBucket: 'fir-ssr-demo.appspot.com',
+  messagingSenderId: '168456548750',
+  appId: '1:168456548750:web:d57cf16364cd765f2674df',
+  measurementId: 'G-KW2KCP0Y33',
+};
